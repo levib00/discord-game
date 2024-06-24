1d6a3cd1386a1ea358edd1b14d028595989d59b2
Levi B <levib2000@gmail.com>
2024-06-24 03:05:44 -0400

    Add connectionto database

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { neon } = require('@neondatabase/serverless');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const sql = neon(`postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
