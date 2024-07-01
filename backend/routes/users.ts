470996d56c2bce9ff9f3287d13e5bc2330b04ab5
Levi B <levib2000@gmail.com>
2024-07-01 18:44:25 -0400

    Add route to for test

--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express';
+import { test } from '../controllers/game';
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(_req: Request, res: Response) {
   res.send('respond with a resource');
 });
+
+router.get('/test', test)
 
 export default router;
