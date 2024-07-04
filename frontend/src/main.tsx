c89548f207f4b797bde2457cee989b5411bba40a
Levi B <levib2000@gmail.com>
2024-07-04 03:11:39 -0400

    Add observer for DOM head

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { headObserver } from './helpers/element-observers'
+
+headObserver()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
