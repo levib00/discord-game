358c2a91bfc780cca2c9f5a21e6cf8e47ba6dade
Levi B <levib2000@gmail.com>
2024-07-04 03:08:14 -0400

    Fix test

--- frontend/src/tests/aim-board.test.tsx
+++ frontend/src/tests/aim-board.test.tsx
@@ -1,16 +1,22 @@
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import AimBoard from '../components/aim-board';
+
+const targets = [{
+  xCoords: 250,
+  yCoords: 250
+}]
 
 describe('AimBoard renders', () => {
   test('About renders with correct text', () => {
     render(
       <MemoryRouter>
-        <AimBoard />
+        <AimBoard targets={targets} />
       </MemoryRouter>,
     );
 
-    const sourceCode = screen.getAllByTestId('aim-board');
-    expect(sourceCode).toBeInTheDocument();
+    const aimBoardId = screen.getByTestId('aim-board');
+    expect(aimBoardId).toBeInTheDocument();
   });
 });
