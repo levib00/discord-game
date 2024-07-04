b69cb1230e3dcc1ef6b0dcfb3ddb484700c8771a
Levi B <levib2000@gmail.com>
2024-07-04 03:12:12 -0400

    Add basic scoreboard

---
+++ frontend/src/components/score-board.tsx
@@ -0,0 +1,21 @@
+interface IScoreBoard {
+  player1Score: number
+  player2Score: number
+}
+
+const ScoreBoard = (props: IScoreBoard) => {
+  const { player1Score, player2Score } = props
+
+  return (
+    <div>
+      <div>
+        <div>Player 1: {player1Score}</div>
+      </div>
+      <div>
+        <div>Player 2: {player2Score}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ScoreBoard;
