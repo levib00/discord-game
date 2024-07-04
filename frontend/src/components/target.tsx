c1f9916f5febb90d14058cf6387f9c413e3b415e
Levi B <levib2000@gmail.com>
2024-07-04 03:12:48 -0400

    Add test id to target

--- frontend/src/components/target.tsx
+++ frontend/src/components/target.tsx
@@ -9,7 +9,7 @@ const Target = (props: ITargetProps) => {
   const {xCoords, yCoords} = props.target;
   
   return (
-    <div style={{position: 'relative', left: xCoords, top: yCoords}}>
+    <div style={{position: 'relative', left: xCoords, top: yCoords}} data-testid='target'>
       
     </div>
   );
