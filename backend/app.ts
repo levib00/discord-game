e69a9c51dca08801c2f01614c08141ff886d0c07
Levi B <levib2000@gmail.com>
2024-07-09 02:41:29 -0400

    Add cors

--- backend/app.ts
+++ backend/app.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { neon } from '@neondatabase/serverless';
+import cors from 'cors';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
@@ -29,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
