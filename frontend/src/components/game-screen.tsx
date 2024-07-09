6e069c175b64348e85fdb7066cc6800dade33e06
Levi B <levib2000@gmail.com>
2024-07-09 02:59:00 -0400

    Get targets from server before rendering

--- frontend/src/components/game-screen.tsx
+++ frontend/src/components/game-screen.tsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from 'react';
 import AimBoard from './aim-board';
 import ScoreBoard from './score-board';
 import StartModal from './start-modal';
 
-const GameScreen = () => (
+const GameScreen = () => {
+  const [targets, setTargets] = useState([]);
+
+  useEffect(() => {
+    const getFetcher = async (url: string) => {
+      try {
+        const data = await fetch(url, {
+          method: 'GET',
+          // @ts-ignore
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          mode: 'cors',
+        });
+
+        return await data.json();
+      } catch (error: any) {
+        throw new Error(error);
+      }
+    };
+    (async () => {
+      const test = await getFetcher('http://localhost:8082/api/game/targets');
+      setTargets(await test);
+    })();
+  }, []);
+
+  return (
     <div data-testid='game-screen'>
       <ScoreBoard player1Score={0} player2Score={0}/>
       <StartModal />
-      <AimBoard />
+      {targets?.length > 0 && <AimBoard targets={targets}/>}
     </div>
-);
+  );
+};
 
 export default GameScreen;
