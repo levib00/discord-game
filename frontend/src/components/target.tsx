a9b172ebd846c0f8b55c9952f7abad695af3aef1
Levi B <levib2000@gmail.com>
2024-07-09 02:59:38 -0400

    Add target behavior and styling

--- frontend/src/components/target.tsx
+++ frontend/src/components/target.tsx
@@ -1,15 +1,52 @@
+export interface ICoordinates {
+  xCoords: number,
+  yCoords: number
+}
+
 interface ITargetProps {
   target: {
     xCoords: number,
     yCoords: number
-  }
+  },
+  setCurrentTargets: React.Dispatch<React.SetStateAction<ICoordinates[]>>,
+  index: number,
+  currentTargets: ICoordinates[],
+  targetClicked: () => void
 }
 
 const Target = (props: ITargetProps) => {
-  const { xCoords, yCoords } = props.target;
+  const {
+    target,
+    index,
+    setCurrentTargets,
+    currentTargets,
+    targetClicked,
+  } = props;
+  const { xCoords, yCoords } = target;
+
+  const removeThisTarget = () => {
+    const currentTargetsCopy = [...currentTargets];
+    currentTargetsCopy.splice(index, 1);
+    setCurrentTargets(currentTargetsCopy);
+  };
+
+  const handleTargetClicked = () => {
+    targetClicked();
+    removeThisTarget();
+  };
 
   return (
-    <div style={{ position: 'relative', left: xCoords, top: yCoords }} data-testid='target'>
+    <div style={{
+      position: 'absolute',
+      left: xCoords + 460,
+      top: yCoords + 60,
+      backgroundColor: 'red',
+      width: '60px',
+      height: '60px',
+      borderRadius: '60px',
+    }} data-testid='target'
+    onClick={handleTargetClicked}
+    >
 
     </div>
   );
