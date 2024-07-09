c06d28dd7864fc5f4fdb05def28160950c7c8b9e
Levi B <levib2000@gmail.com>
2024-07-09 02:45:02 -0400

    Add dark mode logic and tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,30 @@
-function App() {
-  return (
-    <>
+import { useEffect, useState } from 'react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import GameScreen from './components/game-screen';
 
-    </>
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState<string>('true');
+
+  // Load dark mode
+  useEffect(() => {
+    (async () => {
+      const darkModeStorage = localStorage.getItem('darkMode');
+      setIsDarkMode(darkModeStorage || '');
+    })();
+  }, []);
+
+  return (
+    <div className={isDarkMode ? 'top-container dark-mode' : 'top-container'}>
+      <MemoryRouter>
+        <header>
+        </header>
+        <main className='main'>
+          <Routes>
+            <Route path='/' element={<GameScreen />} />
+          </Routes>
+        </main>
+      </MemoryRouter>
+    </div>
   );
 }
 
