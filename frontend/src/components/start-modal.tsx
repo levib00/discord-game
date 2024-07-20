6721bc1efc7762ce0c520e581a862538c9b5f213
Levi B <levib2000@gmail.com>
2024-07-19 21:47:15 -0400

    Connect to socketio namespace on ready

--- frontend/src/components/start-modal.tsx
+++ frontend/src/components/start-modal.tsx
@@ -1,11 +1,18 @@
-const StartModal = () => {
-  const sendReady = () => {
+import { io } from 'socket.io-client';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import socketio from '../socketio';
 
+const StartModal = () => {
+  const [lobbyCode] = useState(useParams().lobbyId);
+
+  const sendReady = async () => {
+    socketio.emit('ready', lobbyCode);
   };
 
   const readyButtonHandler = () => {
     sendReady();
-    // TODO: show this user as ready on client side.
+    io(`http://localhost:8082/${lobbyCode}`);
   };
 
   return (
