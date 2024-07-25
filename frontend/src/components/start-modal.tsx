a97ff05d390237bfda9a1fd6bb2756a3b61adf7a
Levi B <levib2000@gmail.com>
2024-07-25 03:40:00 -0400

    Change namespace connect to state in parent

--- frontend/src/components/start-modal.tsx
+++ frontend/src/components/start-modal.tsx
@@ -1,18 +1,25 @@
 import { io } from 'socket.io-client';
-import { useState } from 'react';
+import { SetStateAction, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import socketio from '../socketio';
 
-const StartModal = () => {
+interface IStartModalProps {
+  setLobbyNsp: SetStateAction<any>
+  lobbyNsp: ReturnType<typeof io>
+}
+
+const StartModal = (props: IStartModalProps) => {
+  const { setLobbyNsp, lobbyNsp } = props;
   const [lobbyCode] = useState(useParams().lobbyId);
 
   const sendReady = async () => {
-    socketio.emit('ready', lobbyCode);
+    lobbyNsp.emit('ready', lobbyCode);
   };
 
   const readyButtonHandler = () => {
-    sendReady();
-    io(`http://localhost:8082/${lobbyCode}`);
+    if (!lobbyNsp?.connected) {
+      setLobbyNsp(io(`http://localhost:8082/${lobbyCode}`));
+      sendReady();
+    }
   };
 
   return (
