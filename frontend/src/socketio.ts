f4de2fd715d0035e1352473bb98d54dbcd4603f4
Levi B <levib2000@gmail.com>
2024-07-25 03:34:47 -0400

    Remove unused code

--- frontend/src/socketio.ts
+++ frontend/src/socketio.ts
@@ -1,8 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8082';
+const URL = 'http://localhost:8082';
 
-// @ts-ignore
-export default io(URL, {
-  autoConnect: false,
-});
+export default io(URL);
