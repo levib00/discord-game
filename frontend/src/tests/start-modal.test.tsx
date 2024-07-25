0c40d3b06c348eedc42b74ff1008b452146bad2c
Levi B <levib2000@gmail.com>
2024-07-25 03:35:46 -0400

    Add some mock props

--- frontend/src/tests/start-modal.test.tsx
+++ frontend/src/tests/start-modal.test.tsx
@@ -1,13 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
+import { io } from 'socket.io-client';
 import StartModal from '../components/start-modal';
 
 describe('Start Modal renders', () => {
   test('Target renders with correct text', () => {
     render(
       <MemoryRouter>
-        <StartModal />
+        <StartModal lobbyNsp={io()} setLobbyNsp={jest.fn()} />
       </MemoryRouter>,
     );
 
