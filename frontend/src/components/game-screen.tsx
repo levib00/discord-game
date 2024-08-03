b1707d7481de35eed0834d9cb0f82594394b6aac
Levi B <levib2000@gmail.com>
2024-08-03 01:29:51 -0400

    Refactor to use react query and add loading

--- frontend/src/components/game-screen.tsx
+++ frontend/src/components/game-screen.tsx
@@ -1,31 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import AimBoard from './aim-board';
 import ScoreBoard from './score-board';
 import StartModal from './start-modal';
 import { getTargets } from '../helpers/fetchers';
 
 const GameScreen = () => {
-  const [targets, setTargets] = useState([]);
   const [score, setScore] = useState(0);
   const [lobbyNsp, setLobbyNsp] = useState<any>();
 
-  useEffect(() => {
-    (async () => {
-      const test = await getTargets('http://localhost:8082/api/game/targets');
-      setTargets(await test);
-    })();
-  }, []);
+  const {
+    isPending, data,
+  } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: async () => {
+      const response = await getTargets('http://localhost:8082/api/game/targets');
+      return response;
+    },
+  });
 
   return (
     <div data-testid='game-screen'>
       <ScoreBoard player1Score={score} player2Score={0}/>
       <StartModal lobbyNsp={lobbyNsp} setLobbyNsp={setLobbyNsp} />
-      {targets?.length > 0 && <AimBoard
+      {data?.length > 0 && <AimBoard
         lobbyNsp={lobbyNsp}
-        targets={targets}
+        targets={data}
         setScore={setScore}
         score={score}
-      />}
+      /> }
+      {isPending && <>Loading...</>}
     </div>
   );
 };
