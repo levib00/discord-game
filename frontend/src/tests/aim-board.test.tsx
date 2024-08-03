a6d265ff3ec99fd22bdfd83e2098cc7e7a76d364
Levi B <levib2000@gmail.com>
2024-08-03 01:13:43 -0400

    Add test for function call on target click

--- frontend/src/tests/aim-board.test.tsx
+++ frontend/src/tests/aim-board.test.tsx
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { io } from 'socket.io-client';
+import userEvent from '@testing-library/user-event';
 import AimBoard from '../components/aim-board';
 
 const targets = [{
@@ -10,6 +11,8 @@ const targets = [{
 }];
 
 describe('AimBoard renders', () => {
+  const emitMock = jest.fn();
+
   test('About renders with correct text', () => {
     render(
       <MemoryRouter>
@@ -19,5 +22,20 @@ describe('AimBoard renders', () => {
 
     const aimBoardId = screen.getByTestId('aim-board');
     expect(aimBoardId).toBeInTheDocument();
+  });
+
+  test('About renders with correct text', async () => {
+    const ioMock = () => ({ emit: emitMock });
+
+    render(
+      <MemoryRouter>
+        {/* @ts-ignore */}
+        <AimBoard targets={targets} score={0} lobbyNsp={ioMock()} setScore={jest.fn()}/>
+      </MemoryRouter>,
+    );
+
+    const target = screen.getByTestId('target');
+    await userEvent.click(target);
+    expect(emitMock).toHaveBeenCalledWith('score', { jwt: '', newScore: 100 });
   });
 });
