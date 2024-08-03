9ce32f9c09c6757a179b5c634a76485d3cb0b169
Levi B <levib2000@gmail.com>
2024-08-03 01:11:31 -0400

    Add react query/test for whether aimboard renders

--- frontend/src/tests/game-screen.test.tsx
+++ frontend/src/tests/game-screen.test.tsx
@@ -1,17 +1,57 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import GameScreen from '../components/game-screen';
+import * as getTargets from '../helpers/fetchers';
+
+const queryClient = new QueryClient();
 
 describe('GameScreen renders', () => {
-  test('Target renders with correct text', () => {
+  test('Gamescreen renders', () => {
     render(
-      <MemoryRouter>
-        <GameScreen />
-      </MemoryRouter>,
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <GameScreen />
+        </MemoryRouter>
+      </QueryClientProvider>,
     );
 
     const gameScreenTitle = screen.getByTestId('game-screen');
     expect(gameScreenTitle).toBeInTheDocument();
+  });
+
+  test('Aimboard does not render if there are no targets', () => {
+    jest.spyOn(getTargets, 'getTargets').mockResolvedValue(() => []);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <GameScreen />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    const aimboard = screen.queryByTestId('aim-board');
+    expect(aimboard).not.toBeInTheDocument();
+  });
+
+  test('Aimboard does not render if there are no targets', async () => {
+    jest.spyOn(getTargets, 'getTargets').mockResolvedValue([{
+      xCoords: 250,
+      yCoords: 250,
+    }]);
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <GameScreen />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      const exampleName1 = screen.getByTestId('aim-board');
+      expect(exampleName1).toBeInTheDocument();
+    });
   });
 });
