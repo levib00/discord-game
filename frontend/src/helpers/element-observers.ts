c76c1437d1db702829748fd813d197bba0a01a6e
Levi B <levib2000@gmail.com>
2024-08-08 05:42:08 -0400

    Linting and comments

--- frontend/src/helpers/element-observers.ts
+++ frontend/src/helpers/element-observers.ts
@@ -1,22 +1,22 @@
+// TODO: Make actually do something.
 export const makeObserver = (target: HTMLElement) => {
-  const observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function() {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach(() => {
       console.log('style changed!');
-    });    
+    });
   });
 
-  observer.observe(target, { attributes : true, attributeFilter : ['style'] });
-}
+  observer.observe(target, { attributes: true, attributeFilter: ['style'] });
+};
 
 export const headObserver = () => {
-  const observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-      for(let i = 0; i < mutation.addedNodes.length; i++)
-        console.log(mutation.addedNodes[i]);
-    })
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      for (let i = 0; i < mutation.addedNodes.length; i += 1) console.log(mutation.addedNodes[i]);
+    });
   });
-  
+
   observer.observe(document.getElementById('head')!, {
-    childList: true
+    childList: true,
   });
-}
+};
