84dfc5d4c043cf761b12a6d068c84abe5002437c
Levi B <levib2000@gmail.com>
2024-08-08 05:24:41 -0400

    Test that score listener is started & fix props

--- frontend/src/tests/score-board.test.tsx
+++ frontend/src/tests/score-board.test.tsx
@@ -4,10 +4,28 @@ import '@testing-library/jest-dom';
 import ScoreBoard from '../components/score-board';
 
 describe('Score Board renders', () => {
-  test('Target renders with correct text', () => {
+  let emitMock: jest.Mock;
+  let onMock: jest.Mock;
+
+  let ioMock: () => {
+    emit: jest.Mock,
+    on: jest.Mock
+  };
+
+  beforeEach(() => {
+    emitMock = jest.fn();
+    onMock = jest.fn();
+
+    ioMock = () => ({
+      emit: emitMock,
+      on: onMock,
+    });
+  });
+
+  test('scoreboard renders with correct text', () => {
     render(
       <MemoryRouter>
-        <ScoreBoard player1Score={0} player2Score={0} />
+        <ScoreBoard player1Score={0} lobbyNsp={ioMock()} playerId='' />
       </MemoryRouter>,
     );
 
@@ -15,5 +33,16 @@ describe('Score Board renders', () => {
     expect(player1Score).toBeInTheDocument();
     const player2Score = screen.getByText('Player 2: 0');
     expect(player2Score).toBeInTheDocument();
+  });
+
+  test('Scoreboard fires function to listen for socket score event', () => {
+    const nspMock = ioMock();
+    render(
+      <MemoryRouter>
+        <ScoreBoard player1Score={0} lobbyNsp={nspMock} playerId='' />
+      </MemoryRouter>,
+    );
+
+    expect(onMock).toHaveBeenCalledTimes(1);
   });
 });
