e4ac3893624f4bafb2bcb8044ed78a2cfe06d1c8
Levi B <levib2000@gmail.com>
2024-08-08 05:38:59 -0400

    Add logic for updating player2Score

--- frontend/src/components/score-board.tsx
+++ frontend/src/components/score-board.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
+import { updatePlayer2Score } from '../helpers/socket-callbacks';
+
 interface IScoreBoard {
   player1Score: number
-  player2Score: number
+  lobbyNsp: any
+  playerId: string
 }
 
 const ScoreBoard = (props: IScoreBoard) => {
-  const { player1Score, player2Score } = props;
+  const {
+    player1Score,
+    lobbyNsp,
+    playerId,
+  } = props;
+
+  const [player2Score, setPlayer2Score] = useState<number>(0);
+
+  // Set other players score. The score the player sees for themself is calculated client side.
+  useEffect(() => {
+    if (lobbyNsp) {
+      lobbyNsp.on('score', (data: any) => {
+        updatePlayer2Score(data, playerId, setPlayer2Score);
+      });
+    }
+  });
 
   return (
     <div>
