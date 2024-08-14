e65fb73ffb2c6cf5114f2c0c378e8de667b183e9
Levi B <levib2000@gmail.com>
2024-08-14 05:27:04 -0400

    Add function to check if lobby nsp exists

--- frontend/src/helpers/fetchers.ts
+++ frontend/src/helpers/fetchers.ts
@@ -1,8 +1,24 @@
-// eslint-disable-next-line import/prefer-default-export
 export const getTargets = async (url: string) => {
   try {
     const data = await fetch(url, {
       method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
+    });
+
+    return await data.json();
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
+
+export const checkLobbyExists = async (url: string) => {
+  try {
+    const data = await fetch(url, {
+      method: 'HEAD',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
