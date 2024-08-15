2897f67c3e4ba5152a79f18f2d96da9801c91dd7
Levi B <levib2000@gmail.com>
2024-08-15 04:29:38 -0400

    Add a test for checkLobbyExists

--- frontend/src/tests/helpers/fetchers.test.ts
+++ frontend/src/tests/helpers/fetchers.test.ts
@@ -1,9 +1,13 @@
-import { getTargets } from '../../helpers/fetchers';
+import { checkLobbyExists, getTargets } from '../../helpers/fetchers';
 // @ts-ignore
 global.fetch = jest.fn(() => Promise.resolve({
   json: () => Promise.reject(),
 }));
 
-test('About renders with correct text', () => {
+test('Errors on getTargets are caught', () => {
   expect(() => getTargets('localhost:3000')).rejects.toThrow(Error);
+});
+
+test('Errors on checkLobbyExists are caught', () => {
+  expect(() => checkLobbyExists('localhost:3000')).rejects.toThrow(Error);
 });
