af9bb4502dd332352e26fc2d0a98e6bb175c0f42
Levi B <levib2000@gmail.com>
2024-11-08 04:20:08 -0500

    Updates

--- src/testttt.js
+++
@@ -1,8 +0,0 @@
-const observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function() {
-      console.log('style changed!');
-  });    
-});
-
-const target = document.getElementById('html');
-observer.observe(target, { attributes : true, attributeFilter : ['style'] });
